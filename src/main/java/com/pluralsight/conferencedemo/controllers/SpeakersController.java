a2de5bd523b832210ca3da2e61c1f37de648c2f9
dacy <darcy@example.com>
2022-12-21 22:46:34 +0800

    implement speaker api

--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -2,11 +2,9 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,19 @@ public class SpeakersController {
     @RequestMapping("{id}")
     public Speaker getOne(@PathVariable Long id) {
         return speakerRepository.getOne(id);
+    }
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
     }
 }
